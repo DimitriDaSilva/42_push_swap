81a0ceda13c210e89bbbc12324bcbdf4f123f32b
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-04-08 14:41:50 +0100

    Test

--- build/src/checker/main.d
+++
@@ -1,6 +0,0 @@
-build/src/checker/main.o: src/checker/main.c src/checker/main.h \
- src/checker/./../../libft/src/libft.h
-
-src/checker/main.h:
-
-src/checker/./../../libft/src/libft.h:
--- build/src/push_swap/main.d
+++
@@ -1,6 +0,0 @@
-build/src/push_swap/main.o: src/push_swap/main.c src/push_swap/main.h \
- src/push_swap/./../../libft/src/libft.h
-
-src/push_swap/main.h:
-
-src/push_swap/./../../libft/src/libft.h:
