e72f53f67b0f037832f6419debc4c068b897f522
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-04-15 20:04:30 +0100

    Correctly sorting 1/4 of 100 stack

--- src/push_swap/sort_stack_large.c
+++ src/push_swap/sort_stack_large.c
@@ -12,21 +12,123 @@
 
 #include "sort_stack_large.h"
 
+/*
+** For larger stacks (5 and more), the logics works as follows:
+** 1. Split stack A in 2 - above median in A & below median in B
+** 2. Merge stack B into A
+**    - First push to A values above 1st quartile and min values
+**    - Second, push to A values below 1st quartile
+** 3. In a loop, by increments of 12, parse the stack A and sort it using
+**    the stack B
+** @param:	- [t_list **] head of the list stack_a
+**			- [t_list **] head of the list stack_b
+** Line-by-line comments:
+** @line-line	comment
+*/
+
 void	sort_stack_large(t_list **stack_a, t_list **stack_b)
 {
 	int	median;
 	int	first_qrtl;
+	int	third_qrtl;
+	int	data_missing;
 
 	median = ft_lst_get_median(*stack_a);
 	first_qrtl = ft_lst_get_qrtl(*stack_a, 1);
+	third_qrtl = ft_lst_get_qrtl(*stack_a, 3);
 	split_a_in_two_w_median(stack_a, stack_b, median, is_less_than_or_equal);
 	merge_b_into_a_w_median(stack_a, stack_b, first_qrtl, is_less_than);
-	while (is_first_node_a_sorted(*stack_a))
+		while (is_first_node_a_sorted(*stack_a))
+			rotate_stack_print(stack_a, "ra");
+		data_missing = get_data_missing(*stack_a);
+		while ((long int)(*stack_a)->data != data_missing)
+			push_stack_print(stack_b, stack_a, "pb");
 		rotate_stack_print(stack_a, "ra");
-	while ((long int)(*stack_a)->data < first_qrtl)
-		push_stack_print(stack_b, stack_a, "pb");
-	merge_b_into_a_in_order(stack_a, stack_b);
+		while ((long int)(*stack_a)->data <= first_qrtl)
+			push_stack_print(stack_b, stack_a, "pb");
+		merge_b_into_a_in_order(stack_a, stack_b);
+		// while (is_first_node_a_sorted(*stack_a))
+		// 	rotate_stack_print(stack_a, "ra");
+		// while ((long int)(*stack_a)->data < first_qrtl)
+		// 	push_stack_print(stack_b, stack_a, "pb");
+		// merge_b_into_a_in_order(stack_a, stack_b);
+	// while ((long int)(*stack_a)->data < median)
+	// {
+	// 	while (is_first_node_a_sorted(*stack_a))
+	// 		rotate_stack_print(stack_a, "ra");
+	// 	while ((long int)(*stack_a)->data < median)
+	// 		push_stack_print(stack_b, stack_a, "pb");
+	// 	merge_b_into_a_in_order(stack_a, stack_b);
+	// }
+	// while ((long int)(*stack_a)->data < third_qrtl)
+	// {
+	// 	while (is_first_node_a_sorted(*stack_a))
+	// 		rotate_stack_print(stack_a, "ra");
+	// 	while ((long int)(*stack_a)->data < third_qrtl)
+	// 		push_stack_print(stack_b, stack_a, "pb");
+	// 	merge_b_into_a_in_order(stack_a, stack_b);
+	// }
 	rotate_until_sorted(stack_a);
+	(void)third_qrtl;
+}
+
+static int	get_data_missing(t_list *stack_a)
+{
+	t_list	*tmp;
+	t_list	*dup;
+	void	*last_node_data;
+	int		data_missing;
+
+	last_node_data = ft_lst_get_data_last_node(stack_a);
+	dup = ft_lstdup(stack_a);
+	ft_lst_sort(&dup, ascending);
+	tmp = dup;
+	while (tmp->data != last_node_data)
+		tmp = tmp->next;
+	data_missing = (long int)tmp->next->data;
+	ft_lstclear(&dup, ft_lstdel_int);
+	return (data_missing);
+}
+
+static void	merge_b_into_a_w_median(t_list **stack_a,
+				t_list **stack_b,
+				int first_qrtl,
+				int (*cmp)(int, int))
+{
+	while (*stack_b)
+	{
+		if ((long int)(*stack_b)->data == ft_lst_get_min(*stack_b))
+		{
+			push_stack_print(stack_a, stack_b, "pa");
+			if (*stack_b != 0)
+				rotate_stack_print(stack_a, "ra");
+		}
+		else if (cmp(first_qrtl, (long int)(*stack_b)->data)
+			|| ((first_qrtl / 2) <= (long int)(*stack_b)->data
+					&& ft_lst_get_max(*stack_b) <= first_qrtl))
+			push_stack_print(stack_a, stack_b, "pa");
+		else
+			rotate_stack_print(stack_b, "rb");
+	}
+}
+
+static int	is_first_node_a_sorted(t_list *stack_a)
+{
+	t_list	*dup;
+	int		index_first_node;
+	int		index_last_node;
+	void	*data_last_node;
+
+	data_last_node = ft_lst_get_data_last_node(stack_a);
+	dup = ft_lstdup(stack_a);
+	ft_lst_sort(&dup, ascending);
+	index_first_node = ft_lst_get_node_index(dup, (long long)stack_a->data);
+	index_last_node = ft_lst_get_node_index(dup, (long long)data_last_node);
+	ft_lstclear(&dup, ft_lstdel_int);
+	if (index_last_node == (index_first_node - 1))
+		return (1);
+	else
+		return (0);
 }
 
 static void	merge_b_into_a_in_order(t_list **stack_a, t_list **stack_b)
@@ -76,45 +178,4 @@ static int	is_min_max_closer_to_top(t_list *stack, int min, int max)
 	else
 		check = (index_max < (len - index_min));
 	return (check);
-}
-
-static void	merge_b_into_a_w_median(t_list **stack_a,
-				t_list **stack_b,
-				int first_qrtl,
-				int (*cmp)(int, int))
-{
-	while (*stack_b)
-	{
-		if ((long int)(*stack_b)->data == ft_lst_get_min(*stack_b))
-		{
-			push_stack_print(stack_a, stack_b, "pa");
-			if (*stack_b != 0)
-				rotate_stack_print(stack_a, "ra");
-		}
-		else if (cmp(first_qrtl, (long int)(*stack_b)->data)
-			|| ((first_qrtl / 2) <= (long int)(*stack_b)->data
-					&& ft_lst_get_max(*stack_b) <= first_qrtl))
-			push_stack_print(stack_a, stack_b, "pa");
-		else
-			rotate_stack_print(stack_b, "rb");
-	}
-}
-
-static int	is_first_node_a_sorted(t_list *stack_a)
-{
-	t_list	*dup;
-	int		index_first_node;
-	int		index_last_node;
-	void	*data_last_node;
-
-	data_last_node = ft_lst_get_data_last_node(stack_a);
-	dup = ft_lstdup(stack_a);
-	ft_lst_sort(&dup, ascending);
-	index_first_node = ft_lst_get_node_index(dup, (long long)stack_a->data);
-	index_last_node = ft_lst_get_node_index(dup, (long long)data_last_node);
-	ft_lstclear(&dup, ft_lstdel_int);
-	if (index_last_node == (index_first_node - 1))
-		return (1);
-	else
-		return (0);
 }
--- src/push_swap/sort_stack_large.h
+++ src/push_swap/sort_stack_large.h
@@ -6,12 +6,14 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 16:45:28 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/04/15 16:47:57 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/04/15 19:04:24 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SORT_STACK_LARGE_H
 # define SORT_STACK_LARGE_H
+
+# define STACK_INCREMENT 12
 
 # include "sort_stack.h"
 
@@ -22,5 +24,6 @@ static void	merge_b_into_a_w_median(t_list **stack_a,
 static int	is_first_node_a_sorted(t_list *stack_a);
 static void	merge_b_into_a_in_order(t_list **stack_a, t_list **stack_b);
 static int	is_min_max_closer_to_top(t_list *stack, int min, int max);
+static int	get_data_missing(t_list *stack_a);
 
 #endif
